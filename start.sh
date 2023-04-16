68a5aec8c49120200dd420f2a8ea07bf018f7614
Just_a_kartik <85619363+IndianDevInc@users.noreply.github.com>
2023-04-16 14:30:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ apt install php -y
 apt install nodejs -y
 apt install npm -y
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
-npm i pm2 -g
-pm2 start web.sh
 # Minecraft version
 VERSION=1.18.2
 BUILD=216
